922b5dd54ea922be5980f3f274fb422ac728f390
prosenjit005 <117080260+MyItWorldPro@users.noreply.github.com>
2023-02-26 10:12:56 +0530

    initial commit

---
+++ JwtRoles1/src/main/java/com/misc/JwtRoles1/JwtRoles1Application.java
@@ -0,0 +1,13 @@
+package com.misc.JwtRoles1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRoles1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtRoles1Application.class, args);
+	}
+
+}
